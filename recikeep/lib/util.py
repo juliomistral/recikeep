ef71e2b07ddd965a15517f76ee063b4017a2e51d
Julio A. Mistral <jmistral@grio.com>
2013-02-09 00:45:20 -0800

    Add comments to the clen split lines util function

--- recikeep/lib/util.py
+++ recikeep/lib/util.py
@@ -1,4 +1,7 @@
 def clean_split_lines(lines):
+    """
+    Cleans the list of any entries that are nothing by white-space
+    """
     return [
         line.rstrip().lstrip()
             for line in lines if len(line.rstrip().lstrip()) != 0
