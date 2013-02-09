eae1303fd83a3ebede88b729d9d153f9e19072f0
Julio A. Mistral <jmistral@grio.com>
2013-02-09 15:41:56 -0800

    Rename user management sub-path to "users"

--- recikeep/urls.py
+++ recikeep/urls.py
@@ -8,7 +8,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^user/', include('recikeep.usermanagement.urls')),
+    url(r'^users/', include('recikeep.usermanagement.urls')),
+    url(r'^recipes/', include('recikeep.recipes.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
